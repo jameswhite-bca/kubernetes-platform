6205cdb9c9f6099298d31fbac724d2711060244d
jameswhite-bca <james.white1@bca.com>
2025-12-30 15:54:46 +0000

    fix: correct version reference in virtual network module source

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -1,6 +1,6 @@
 # Creating a virtual network with a unique name, telemetry settings, and in the specified resource group and location.
 module "vnet" {
-  source = "git::https://github.com/Azure/terraform-azurerm-avm-res-network-virtualnetwork.git?ref=0.16.0"
+  source = "git::https://github.com/Azure/terraform-azurerm-avm-res-network-virtualnetwork.git?ref=v0.16.0"
 
   location         = var.azrm_resource_location
   parent_id        = module.resource_group.id
