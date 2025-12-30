75e6d452d3bf2cc060a175805aed2d426dd295e1
jameswhite-bca <james.white1@bca.com>
2025-12-30 15:53:25 +0000

    add networking module for virtual network creation

---
+++ terraform/networking.tf
@@ -0,0 +1,10 @@
+# Creating a virtual network with a unique name, telemetry settings, and in the specified resource group and location.
+module "vnet" {
+  source = "git::https://github.com/Azure/terraform-azurerm-avm-res-network-virtualnetwork.git?ref=0.16.0"
+
+  location         = var.azrm_resource_location
+  parent_id        = module.resource_group.id
+  address_space    = ["10.0.0.0/16"]
+  enable_telemetry = true
+  name             = module.naming.virtual_network.name
+}
